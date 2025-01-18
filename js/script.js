17319088fb9f5c866bee24f52890c61e7348341f
mjsdesen <marcos_jsantos@msn.com>
2025-01-17 23:31:18 -0300

    Refatora a definição de Web Components para reutilização de código e melhor organização

--- js/script.js
+++ js/script.js
@@ -1,45 +1,25 @@
-// Objetivo: Criar um Web Component para o menu da aplicação
-class MenuComponent extends HTMLElement {
-  async connectedCallback() {
-    // Carregar o arquivo menu.html
-    const templateResponse = await fetch('components/menu.html');
-    const templateHTML = await templateResponse.text();
+class BaseComponent extends HTMLElement {
+  constructor(templatePath) {
+    super();
+    this.templatePath = templatePath;
+  }
 
-    // Inserir o HTML diretamente no componente
+  async connectedCallback() {
+    const templateResponse = await fetch(this.templatePath);
+    const templateHTML = await templateResponse.text();
     this.innerHTML = templateHTML;
   }
 }
-// Definir o Web Component do menu
-customElements.define('menu-component', MenuComponent);
 
-
-// ---------------------------------------------------------
-// objecivo: Criar um Web Component para o head da aplicação
-
-class bodyComponent extends HTMLElement {
-  async connectedCallback() {
-    // Carregar o arquivo head.html
-    const templateResponse = await fetch('/components/body.html');
-    const templateHTML = await templateResponse.text();
-
-    // Inserir o HTML diretamente no componente
-    this.innerHTML = templateHTML;
-  }
+function defineComponent(tagName, templatePath) {
+  customElements.define(tagName, class extends BaseComponent {
+    constructor() {
+      super(templatePath);
+    }
+  });
 }
-// Definir o Web Component do head
-customElements.define('body-component', bodyComponent);
 
-// ---------------------------------------------------------
-// objecivo: Criar um Web Component para o rodape da aplicação
-class footerComponent extends HTMLElement {
-  async connectedCallback() {
-    // Carregar o arquivo rodape.html
-    const templateResponse = await fetch('/components/footer.html');
-    const templateHTML = await templateResponse.text();
-
-    // Inserir o HTML diretamente no componente
-    this.innerHTML = templateHTML;
-  }
-}
-customElements.define('footer-component', footerComponent);
-
+// Definir os Web Components
+defineComponent('menu-component', 'components/menu.html');
+defineComponent('body-component', 'components/body.html');
+defineComponent('footer-component', 'components/footer.html');
